cf37489a6cdcf455f04b53ab5147036286c1d9e8
gumodi10@gmail.com <gumodi10@gmail.com>
2023-01-30 17:48:48 -0600

    Add API to card component

--- pokedux/src/components/PokemonCard.jsx
+++ pokedux/src/components/PokemonCard.jsx
@@ -1,14 +1,20 @@
-import { Card } from "antd";
 import { StarOutlined } from '@ant-design/icons';
+import { Card } from 'antd';
 import Meta from 'antd/lib/card/Meta';
+import './PokemonList.css';
 
-const PokemonCard = () => {
+const PokemonCard = ({ name }) => {
   return (
-    <Card 
-        title="Pikachu"
-        cover={<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png" alt="Pikachu"/>}
-        extra={<StarOutlined />}
-        >
+    <Card
+      title={name}
+      cover={
+        <img
+          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png'
+          alt='Ditto'
+        />
+      }
+      extra={<StarOutlined />}
+    >
       <Meta description='fire, magic' />
     </Card>
   );
